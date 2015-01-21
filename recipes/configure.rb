b1971771cba32ca589e1ae3e7d2e11adcf493262
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2015-01-21 23:58:40 +0200

    Remove vagrant specifics which is not there in the cloud

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -4,15 +4,15 @@
 #
 # Author:: Mike Ensor (<mike.ensor@acquitygroup.com>)
 # Author:: Vince Montalbano (<vince.montalbano@acquitygroup.com>)
-# 
+#
 # Copyright 2013, Acquity Group
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,13 +59,6 @@ end
 execute "set-oracle-environment" do
 	user "root"
 	command "echo \"\n. #{node['oracle-11g-xe'][:oracle_env_path]}\" >> #{bashrc}"
-	notifies :run, "execute[fill-bash-files]", :immediately
-end
-
-execute "fill-bash-files" do
-	user "root"
-	command "cat /home/vagrant/.bash_profile >> #{oracle_bash_profile}; cat /home/vagrant/.bashrc >> #{oracle_bashrc};"
-	action :nothing
 end
 
 execute "stop-listener" do
